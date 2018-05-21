c7e2ae77084ef0e6521e0e30063e533090bde955
Richard B. Winston <rbwinst@usgs.gov>
2018-05-21 16:56:42 -0400

    Initial Code

---
+++ OgwAnalytics.dpr
@@ -0,0 +1,14 @@
+program OgwAnalytics;
+
+uses
+  Vcl.Forms,
+  frmMainUnit in 'frmMainUnit.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
